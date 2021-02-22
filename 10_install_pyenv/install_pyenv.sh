94b6ef1837b561b450c3cdb20ae1f2accf379403
arorakash <akash.arora73elearn@gmail.com>
2021-02-22 21:52:05 +0530

    master: added script to install pyenv and its virtualenv plugin

---
+++ 10_install_pyenv/install_pyenv.sh
@@ -0,0 +1,31 @@
+# install pyenv
+
+# install dependencies
+sudo apt-get update;
+sudo apt-get install --no-install-recommends -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev;
+
+# Set Clone Directory
+CLONE_DIRECTORY=$HOME/.pyenv/;
+git clone https://github.com/pyenv/pyenv.git $CLONE_DIRECTORY;
+
+# Set environment variables in bash
+echo '' >> ~/.bashrc;
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc;
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc;
+echo '' >> ~/.bashrc;
+
+# Set environment variables in zsh
+# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+# echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+
+
+# install pyenv-virtualenv
+
+# Set Clone Directory
+CLONE_DIRECTORY=$HOME/.pyenv/plugins/pyenv-virtualenv;
+git clone https://github.com/pyenv/pyenv-virtualenv.git $CLONE_DIRECTORY;
+
+# Add entries to enable auto-completion and auto-activation of virtualenvs
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\n  eval "$(pyenv virtualenv-init -)"\nfi' >> ~/.bashrc
+echo '' >> ~/.bashrc;
+
