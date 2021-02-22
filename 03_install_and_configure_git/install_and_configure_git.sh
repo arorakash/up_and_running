f6a59a85e73e091cd43ad439f29487e998973986
arorakash <akash.arora73elearn@gmail.com>
2021-02-22 20:04:41 +0530

    master: added scripts to install and configure git and python

---
+++ 03_install_and_configure_git/install_and_configure_git.sh
@@ -0,0 +1,7 @@
+# install git
+sudo apt install git
+
+# configure git user
+git config --global user.name arorakash
+git config --global user.email akash.arora73elearn@gmail.com
+
---
+++ 04_install_python_and_pip/install_python_and_pip.sh
@@ -0,0 +1,17 @@
+# install python
+# python3 already comes shipped with ubuntu
+
+# install pip
+sudo apt install python3-pip -y
+
+# install python-dev
+sudo apt install python3-dev -y
+
+# install python3-venv
+sudo apt install python3-venv -y
+
+# upgrade system pip setuptools and wheels
+sudo python3 -m pip install --upgrade setuptools
+sudo python3 -m pip install --upgrade wheel
+sudo python3 -m pip install --upgrade pip
+
