253e1469ab0bc56e9bfa61989c2da41e9d969af7
arorakash <akash.arora73elearn@gmail.com>
2021-02-26 17:18:11 +0530

    master: fixed another bug in set ssh keys script

--- 01_set_ssh_keys/set_ssh_keys.sh
+++ 01_set_ssh_keys/set_ssh_keys.sh
@@ -13,6 +13,15 @@ echo ""
 # Set Target Directory
 TARGET_DIRECTORY=$HOME/.ssh/
 
+# Creating ~/.ssh/ directory
+if [ -d $TARGET_DIRECTORY ]; then
+    echo "~/.ssh/ directory already exists"
+else
+    echo "~/.ssh/ directory does not exists. creating one"
+    mkdir $TARGET_DIRECTORY;
+fi
+echo ""
+
 # move keypair to target directory
 mv $DOWNLOAD_DIRECTORY/ssh_keypair/* $TARGET_DIRECTORY;
 echo "moved keypair to ~/.ssh/"
