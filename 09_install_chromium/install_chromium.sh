9c5c3493d3442a35322fb7d3a58abd4aa9103cff
arorakash <akash.arora73elearn@gmail.com>
2021-02-27 23:31:01 +0530

    master: added code in chromium installation script to restart snap service to prevent installation getting stuck

--- 09_install_chromium/install_chromium.sh
+++ 09_install_chromium/install_chromium.sh
@@ -1,5 +1,11 @@
 echo ""
 
+# restart snap service
+echo "restarting snap service"
+sudo service snapd restart;
+echo "snap service restarted"
+echo ""
+
 # install chromium
 echo "installing chromium"
 sudo apt install chromium-browser -y
