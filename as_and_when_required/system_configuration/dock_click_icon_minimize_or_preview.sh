cead58d01719f3baf576a72fd03bebca4d7dc647
arorakash <akash.arora73elearn@gmail.com>
2021-02-27 23:56:14 +0530

    master: added script to set dock click icon action to minimize or preview

---
+++ as_and_when_required/system_configuration/dock_click_icon_minimize_or_preview.sh
@@ -0,0 +1,7 @@
+echo ""
+
+# set dock click action to minimize or preview
+echo "setting dock click action to minimize or preview"
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews';
+echo "dock click action set to minimize or preview"
+echo ""
