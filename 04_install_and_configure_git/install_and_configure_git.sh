e3376e84daa27824daca86fbf9b71f0c1172a7d8
arorakash <akash.arora73elearn@gmail.com>
2021-02-28 00:17:23 +0530

    master: added code in git installation script to configure git pager avoid pagination

--- 04_install_and_configure_git/install_and_configure_git.sh
+++ 04_install_and_configure_git/install_and_configure_git.sh
@@ -10,9 +10,21 @@ echo ""
 echo "adding global configuration"
 git config --global user.name arorakash
 git config --global user.email akash.arora73elearn@gmail.com
+git config --global core.pager 'less -FRX'
 echo "global configuration added"
+echo ""
+
+# show global git config
+echo "git global config looks like this"
+echo ""
+cat ~/.gitconfig
+echo ""
 echo ""
 
 echo "clone git repo using ssh as follows"
 echo "git clone git@github.com:arorakash/up_and_running.git"
+echo ""
+
+echo "clone git repo using https as follows"
+echo "wget https://www.github.com/arorakash/up_and_running/archive/master.zip"
 echo ""
