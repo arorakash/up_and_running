258cbe78b437b3659eeb0745036787d78ff6c4cc
arorakash <akash.arora73elearn@gmail.com>
2021-02-28 20:09:51 +0530

    master: modified tools installation script to install net-tools

--- 03_install_tools/install_tools.sh
+++ 03_install_tools/install_tools.sh
@@ -6,6 +6,12 @@ sudo apt install curl -y;
 echo "curl installed"
 echo ""
 
+# install net-tools
+echo "installing net-tools"
+sudo apt install net-tools;
+echo "net-tools installed"
+echo ""
+
 # install diffuse
 echo "installing diffuse"
 sudo apt install diffuse -y;
