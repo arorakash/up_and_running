9ba8f5a69368f19e9bc73f31db679f427c4d2030
arorakash <akash.arora73elearn@gmail.com>
2021-03-01 23:22:10 +0530

    master: added more entries in zsh alias file

--- 14_install_ohmyzsh/aliases.zsh
+++ 14_install_ohmyzsh/aliases.zsh
@@ -1,6 +1,12 @@
-# my custom aliases
+# My custom aliases
 
-alias x='exit';
+alias x="exit"
 
-alias share_current='python3 -m http.server 6006';
-alias share_home='python3 -m http.server 6006 -d $HOME';
+alias upd="sudo apt update;"
+alias fupg="sudo apt full-upgrade -y;"
+alias armv="sudo apt auto-remove -y;"
+
+alias commover="cvlc /home/aa/Music/sounds/notification/just-saying-593.mp3 --play-and-exit &"
+
+alias share_current='python3 -m http.server 6006;'
+alias share_home='python3 -m http.server 6006 -d $HOME;'
