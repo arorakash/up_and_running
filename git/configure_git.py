dca2d306742bef51390ddf5154cd903358055d68
arorakash <akash.arora73elearn@gmail.com>
2021-10-01 19:19:56 +0530

    master: added scripts to install git and configure git

---
+++ git/configure_git.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
+sys.path.append(ROOT_DIR)
+
+import helper
+
+
+def handler(config, common):
+    try:
+        subprocess_command_list = list()
+
+        for key, value in config['global'].items():
+            subprocess_command_list.append(
+                [
+                    "git",
+                    "config",
+                    f"--global",
+                    key,
+                    value,
+                ]
+            )
+
+        for command in subprocess_command_list:
+            command_result = helper.execute_subprocess_command(command)
+
+            if command_result.returncode not in (0,):
+                raise helper.CustomException(str(command_result.stderr))
+
+            helper.sleep(common['sleep_time'])
+
+        return True, 'success'
+
+    except Exception as e:
+        return False, str(e)
