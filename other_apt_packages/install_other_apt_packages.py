103c147217e6373e865b6b60caa7355c40db791d
arorakash <akash.arora73elearn@gmail.com>
2021-10-06 00:14:31 +0530

    master: added script to install other apt packages

---
+++ other_apt_packages/install_other_apt_packages.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
+sys.path.append(ROOT_DIR)
+
+import helper
+
+
+def handler(config, common):
+    try:
+        subprocess_command_list = list()
+
+        for package in config['apt_install']:
+            subprocess_command = helper.make_apt_install_command(
+                install_list=[package],
+                flags=config.get('flags'),
+            )
+
+            subprocess_command_list.append(subprocess_command)
+
+        for command in subprocess_command_list:
+            command_result = helper.execute_subprocess_command(command)
+
+            if command_result.returncode not in (0,):
+                raise helper.CustomException(str(command_result.stderr))
+
+            helper.sleep(common['sleep_time'])
+
+        return True, 'success'
+
+    except Exception as e:
+        return False, str(e)
