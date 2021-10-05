b1c374869615771fe347facd278862c471774ca1
arorakash <akash.arora73elearn@gmail.com>
2021-10-05 23:18:39 +0530

    master: added script to modify gnome behaviour

---
+++ gnome_settings/set_gnome_behaviour.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
+sys.path.append(ROOT_DIR)
+
+import helper
+
+
+def handler(config, common):
+    try:
+        subprocess_command_list = list()
+
+        for behaviour in config['behaviours']:
+            behaviour_target = behaviour['behaviour_target']
+            behaviour_key = behaviour['behaviour_key']
+            behaviour_value = behaviour['behaviour_value']
+
+            subprocess_command = f"gsettings set {behaviour_target} {behaviour_key} '{behaviour_value}'"
+
+            subprocess_command_list.append(subprocess_command)
+
+        for command in subprocess_command_list:
+            command_result = helper.execute_subprocess_command(command)
+
+            if command_result.returncode not in (0,):
+                raise helper.CustomException(str(command_result.stderr))
+
+            helper.sleep(common['sleep_time'])
+
+        return True, 'success'
+
+    except Exception as e:
+        return False, str(e)
