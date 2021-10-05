c57b307454bb711237531e660a1136a0ff803cf2
arorakash <akash.arora73elearn@gmail.com>
2021-10-05 23:12:56 +0530

    master: added script to add user to group

---
+++ vbox_settings/add_user_to_group.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
+sys.path.append(ROOT_DIR)
+
+import helper
+
+
+def handler(config, common):
+    try:
+        user = config['user']
+        group = config['group']
+
+        subprocess_command = f'sudo adduser {user} {group}'
+
+        subprocess_command_list = list()
+        subprocess_command_list.append(subprocess_command)
+
+        for command in subprocess_command_list:
+            command_result = helper.execute_subprocess_command(command)
+
+            if command_result.returncode not in (0,):
+                raise helper.CustomException(str(command_result.stderr))
+
+            helper.sleep(common['sleep_time'])
+
+        return True, 'success'
+
+    except Exception as e:
+        return False, str(e)
