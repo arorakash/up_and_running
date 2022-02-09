8cbbb014a6d3a1a56bbd51ecb13afb0b7bcdff95
arorakash <akash.arora73elearn@gmail.com>
2022-02-09 19:56:42 +0530

    master: added comment to change ownership of vbox shared folder in case of permission issue

--- as_and_when_required/inside_virtual_box_vms/add_user_to_shared_folder_group.sh
+++ as_and_when_required/inside_virtual_box_vms/add_user_to_shared_folder_group.sh
@@ -5,3 +5,8 @@ sudo adduser $USER vboxsf;
 echo $USER" added to vboxsf group"
 echo ""
 
+# check ownership of shared folder using ls -lah /media/<sf>
+
+# https://stackoverflow.com/questions/26740113/virtualbox-shared-folder-permissions/65011188#65011188
+# change group ownership of shared folder to vboxsf if not already
+# sudo chgrp vboxsf /media/<sf>
