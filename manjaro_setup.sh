6876dd57e1e2fd51559db504fd43a3d6e11e1575
arorakash <akash.arora73elearn@gmail.com>
2022-02-28 14:47:15 +0530

    master: fixed minor mistake in echo statements. also added command to refresh package databases

--- manjaro_setup.sh
+++ manjaro_setup.sh
@@ -10,21 +10,33 @@ SLEEP_TIME=3;
 
 
 # generic package management
+echo ""
+echo ""
+echo "updating package databases"
+sudo pacman -Syy;
+echo "package databases updated"
+echo ""
+echo ""
+sleep $SLEEP_TIME;
 
 # Upgrade Packages
-echo "\n"
+echo ""
+echo ""
 echo "updating packages"
-sudo pacman -Syu;
+sudo pacman -Syu --noconfirm;
 echo "packages updated"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # Clean up all local caches.
-echo "\n"
+echo ""
+echo ""
 echo "cleaning local caches"
 sudo pacman -Sc --noconfirm;
 echo "local caches cleaned"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # Remove dependencies that are no longer needed
@@ -37,11 +49,13 @@ sleep $SLEEP_TIME;
 
 
 # install yay package manager
-echo "\n"
+echo ""
+echo ""
 echo "installing yay"
 sudo pacman -S yay --noconfirm;
 echo "yay installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -50,39 +64,41 @@ sleep $SLEEP_TIME;
 
 
 
-# place_ssh_keys
+# # place_ssh_keys
 
-echo "\n"
-echo "placing ssh keys"
+# echo ""
+# echo ""
+# echo "placing ssh keys"
 
-# Set Download Directory
-DOWNLOAD_DIRECTORY=$HOME/Downloads
+# # Set Download Directory
+# DOWNLOAD_DIRECTORY=$HOME/Downloads
 
-# unzip file
-unzip $DOWNLOAD_DIRECTORY/ssh_keypair.zip -d $DOWNLOAD_DIRECTORY;
-sleep $SLEEP_TIME;
+# # unzip file
+# unzip $DOWNLOAD_DIRECTORY/ssh_keypair.zip -d $DOWNLOAD_DIRECTORY;
+# sleep $SLEEP_TIME;
 
-# Set Target Directory
-TARGET_DIRECTORY=$HOME/.ssh/
+# # Set Target Directory
+# TARGET_DIRECTORY=$HOME/.ssh/
 
-# Creating ~/.ssh/ directory
-if [ -d $TARGET_DIRECTORY ]; then
-    echo "~/.ssh/ directory already exists"
-else
-    echo "~/.ssh/ directory does not exists. creating one"
-    mkdir $TARGET_DIRECTORY;
-fi
-sleep $SLEEP_TIME;
+# # Creating ~/.ssh/ directory
+# if [ -d $TARGET_DIRECTORY ]; then
+#     echo "~/.ssh/ directory already exists"
+# else
+#     echo "~/.ssh/ directory does not exists. creating one"
+#     mkdir $TARGET_DIRECTORY;
+# fi
+# sleep $SLEEP_TIME;
 
-# move keypair to target directory
-mv $DOWNLOAD_DIRECTORY/ssh_keypair/* $TARGET_DIRECTORY;
-sleep $SLEEP_TIME;
+# # move keypair to target directory
+# mv $DOWNLOAD_DIRECTORY/ssh_keypair/* $TARGET_DIRECTORY;
+# sleep $SLEEP_TIME;
 
-# Delete downloaded and extracted data
-rm -rf $DOWNLOAD_DIRECTORY/ssh_keypair*;
-echo "ssh keys placed"
-echo "\n"
-sleep $SLEEP_TIME;
+# # Delete downloaded and extracted data
+# rm -rf $DOWNLOAD_DIRECTORY/ssh_keypair*;
+# echo "ssh keys placed"
+# echo ""
+# echo ""
+# sleep $SLEEP_TIME;
 
 
 
@@ -92,7 +108,8 @@ sleep $SLEEP_TIME;
 
 # generate_ssh_keys
 
-echo "\n"
+echo ""
+echo ""
 echo "generating ssh keys"
 
 # install xclip
@@ -114,7 +131,8 @@ sleep $SLEEP_TIME;
 # export public ssh key
 cat ~/.ssh/id_rsa.pub >> $HOME/public_ssh_key.txt
 echo "ssh keys generated"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -126,11 +144,13 @@ sleep $SLEEP_TIME;
 # install_build_tools
 
 # install gcc make
-echo "\n"
+echo ""
+echo ""
 echo "installing build tools"
 sudo pacman -S gcc make --noconfirm;
 echo "build tools installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -142,11 +162,13 @@ sleep $SLEEP_TIME;
 # install_tools
 
 # install curl net-tools meld
-echo "\n"
+echo ""
+echo ""
 echo "installing tools"
 sudo pacman -S curl net-tools meld --noconfirm;
 echo "tools installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -156,21 +178,25 @@ sleep $SLEEP_TIME;
 
 
 # install_git
-echo "\n"
+echo ""
+echo ""
 echo "installing git"
 sudo pacman -S git --noconfirm;
 echo "git installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # configure_git
-echo "\n"
+echo ""
+echo ""
 echo "configuring git"
 git config --global user.name arorakash
 git config --global user.email akash.arora73elearn@gmail.com
 git config --global core.pager 'less -FRX'
 echo "git configured"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -181,25 +207,31 @@ sleep $SLEEP_TIME;
 
 # install_python3_pip
 
-echo "\n"
+echo ""
+echo ""
 echo "installing python-pip"
 sudo pacman -S python-pip --noconfirm;
 echo "python-pip installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
-echo "\n"
+echo ""
+echo ""
 echo "installing python-setuptools"
 sudo pacman -S python-setuptools --noconfirm;
 echo "python-setuptools installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
-echo "\n"
+echo ""
+echo ""
 echo "installing python-wheel"
 sudo pacman -S python-wheel --noconfirm;
 echo "python-wheel installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -213,11 +245,13 @@ sleep $SLEEP_TIME;
 
 
 # install_python_packages
-echo "\n"
+echo ""
+echo ""
 echo "installing tldr"
 python3 -m pip install tldr
 echo "tldr installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -227,11 +261,13 @@ sleep $SLEEP_TIME;
 
 
 # install_chromium
-echo "\n"
+echo ""
+echo ""
 echo "installing chromium"
 sudo pacman -S chromium --noconfirm;
 echo "chromium installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -241,11 +277,13 @@ sleep $SLEEP_TIME;
 
 
 # install_chrome
-echo "\n"
+echo ""
+echo ""
 echo "installing google chrome"
 yay -S google-chrome;
 echo "google chrome installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -256,11 +294,13 @@ sleep $SLEEP_TIME;
 
 # install_sublime_text using snap
 # install_sublime_text using yay
-echo "\n"
+echo ""
+echo ""
 echo "installing sublime text 4"
 yay -S sublime-text-4;
 echo "sublime text 4 installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -271,11 +311,13 @@ sleep $SLEEP_TIME;
 
 # install_pycharm using snap
 # install_pycharm using yay
-echo "\n"
+echo ""
+echo ""
 echo "installing pycharm community"
 yay -S pycharm-community-edition;
 echo "pycharm community installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -285,11 +327,13 @@ sleep $SLEEP_TIME;
 
 
 # change default shell to zsh
-echo "\n"
+echo ""
+echo ""
 echo "changing default shell to zsh"
 chsh -s $(which zsh);
 echo "default shell changed to zsh"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # other shell related commands
@@ -316,11 +360,13 @@ shutdown -r now;
 
 
 # install_ohmyzsh
-echo "\n"
+echo ""
+echo ""
 echo "installing ohmyzsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 echo "ohmyzsh installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -330,7 +376,8 @@ sleep $SLEEP_TIME;
 
 
 # configure_aliases
-echo "\n"
+echo ""
+echo ""
 echo "configuring ohmyzsh aliases"
 
 # set target dir
@@ -339,7 +386,8 @@ TARGET_DIRECTORY=$HOME/.oh-my-zsh/custom/
 # move aliases.zsh to target directory
 cp $PWD/ohmyzsh/aliases.zsh $TARGET_DIRECTORY;
 echo "ohmyzsh aliases configured"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # restarting just in case
@@ -352,7 +400,8 @@ shutdown -r now;
 
 
 # install_nodejs
-echo "\n"
+echo ""
+echo ""
 echo "installing nodejs"
 sudo pacman -S nodejs npm --noconfirm;
 sleep $SLEEP_TIME;
@@ -365,43 +414,52 @@ sleep $SLEEP_TIME;
 
 sudo n lts;
 echo "nodejs installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
 # install_yarn
-echo "\n"
+echo ""
+echo ""
 echo "installing yarn"
 sudo pacman -S yarn --noconfirm;
 echo "yarn installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # install_nvm
-echo "\n"
+echo ""
+echo ""
 echo "installing nvm"
 sudo pacman -S nvm --noconfirm;
 echo "nvm installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # add_nvm_entries
-echo "\n"
+echo ""
+echo ""
 echo "adding nvm entries to bashrc"
 echo '' >> ~/.bashrc;
 echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
 echo '' >> ~/.bashrc;
 echo "nvm entries to bashrc added"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
-echo "\n"
+echo ""
+echo ""
 echo "adding nvm entries to zshrc"
 echo '' >> ~/.zshrc;
 echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
 echo '' >> ~/.zshrc;
 echo "nvm entries to zshrc added"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -411,7 +469,8 @@ sleep $SLEEP_TIME;
 
 
 # install_pyenv
-echo "\n"
+echo ""
+echo ""
 echo "installing pyenv"
 
 # set clone dir
@@ -420,7 +479,8 @@ CLONE_DIRECTORY=$HOME/.pyenv/;
 # cloning pyenv project
 git clone https://github.com/pyenv/pyenv.git $CLONE_DIRECTORY;
 echo "pyenv installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -432,7 +492,8 @@ sleep $SLEEP_TIME;
 # add_pyenv_environment_entries_in_shells_rc
 
 # add in bashrc
-echo "\n"
+echo ""
+echo ""
 echo "adding pyenv entries to bashrc"
 echo '' >> ~/.bashrc;
 echo 'export PYENV_ROOT=$HOME/.pyenv;' >> ~/.bashrc;
@@ -441,11 +502,13 @@ echo 'eval "$(pyenv init --path)";' >> ~/.bashrc;
 echo 'eval "$(pyenv init -)";' >> ~/.bashrc;
 echo '' >> ~/.bashrc;
 echo "pyenv entries to bashrc added"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # add in zshrc
-echo "\n"
+echo ""
+echo ""
 echo "adding pyenv entries to zshrc"
 echo '' >> ~/.zshrc;
 echo 'export PYENV_ROOT=$HOME/.pyenv;' >> ~/.zshrc;
@@ -454,7 +517,8 @@ echo 'eval "$(pyenv init --path)";' >> ~/.zshrc;
 echo 'eval "$(pyenv init -)";' >> ~/.zshrc;
 echo '' >> ~/.zshrc;
 echo "pyenv entries to zshrc added"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -464,7 +528,8 @@ sleep $SLEEP_TIME;
 
 
 # install_pyenv_virtualenv_plugin
-echo "\n"
+echo ""
+echo ""
 echo "installing pyenv virtualenv plugin"
 
 # set clone dir
@@ -473,7 +538,8 @@ CLONE_DIRECTORY=$HOME/.pyenv/plugins/pyenv-virtualenv;
 # cloning pyenv-virtualenv project
 git clone https://github.com/pyenv/pyenv-virtualenv.git $CLONE_DIRECTORY;
 echo "pyenv virtualenv plugin installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -486,23 +552,27 @@ sleep $SLEEP_TIME;
 # add_pyenv_virtualenv_environment_entries_in_shells_rc
 
 # add in bashrc
-echo "\n"
+echo ""
+echo ""
 echo "adding pyenv virtualenv entries in bashrc"
 echo '' >> ~/.bashrc;
 echo 'eval "$(pyenv virtualenv-init -)";' >> ~/.bashrc;
 echo '' >> ~/.bashrc;
 echo "pyenv virtualenv entries in bashrc added"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # add in zshrc
-echo "\n"
+echo ""
+echo ""
 echo "adding pyenv virtualenv entries in zshrc"
 echo '' >> ~/.zshrc;
 echo 'eval "$(pyenv virtualenv-init -)";' >> ~/.zshrc;
 echo '' >> ~/.zshrc;
 echo "pyenv virtualenv entries in zshrc added"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -514,23 +584,27 @@ sleep $SLEEP_TIME;
 # add_pipenv_environment_entries_in_shells_rc
 
 # add in bashrc
-echo "\n"
+echo ""
+echo ""
 echo "adding pipenv entries in bashrc"
 echo '' >> ~/.bashrc;
 echo 'export PIPENV_VENV_IN_PROJECT=1' >> ~/.bashrc;
 echo '' >> ~/.bashrc;
 echo "pipenv entries in bashrc added"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # add in zshrc
-echo "\n"
+echo ""
+echo ""
 echo "adding pipenv entries in zshrc"
 echo '' >> ~/.zshrc;
 echo 'export PIPENV_VENV_IN_PROJECT=1' >> ~/.zshrc;
 echo '' >> ~/.zshrc;
 echo "pipenv entries in zshrc added"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -541,11 +615,13 @@ sleep $SLEEP_TIME;
 
 # install_vscode using snap
 # install_vscode using yay
-echo "\n"
+echo ""
+echo ""
 echo "installing vscode"
 yay -S vscode;
 echo "vscode installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -556,11 +632,13 @@ sleep $SLEEP_TIME;
 
 # install_postman using snap
 # install_postman using yay
-echo "\n"
+echo ""
+echo ""
 echo "installing postman"
 yay -S postman;
 echo "postman installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -570,11 +648,13 @@ sleep $SLEEP_TIME;
 
 
 # add user to vboxsf group
-echo "\n"
+echo ""
+echo ""
 echo "adding user to vboxsf group"
 sudo gpasswd -a $USER vboxsf;
 echo "user to vboxsf group added"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -584,13 +664,15 @@ sleep $SLEEP_TIME;
 
 
 # set_gnome_behaviour
-echo "\n"
+echo ""
+echo ""
 echo "setting gnome behaviour"
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews';
 gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']";
 gsettings get org.gnome.desktop.wm.keybindings minimize "['<Super>z']";
 echo "gnome behaviour set"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # https://askubuntu.com/questions/1298574/troubleshoot-keyboard-shortcut-view-split-on-right
@@ -605,11 +687,13 @@ sleep $SLEEP_TIME;
 
 
 # install_persepolis
-echo "\n"
+echo ""
+echo ""
 echo "installing persepolis"
 sudo pacman -S persepolis --noconfirm;
 echo "persepolis installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -620,11 +704,13 @@ sleep $SLEEP_TIME;
 
 
 # install_telegram
-echo "\n"
+echo ""
+echo ""
 echo "installing telegram"
 sudo pacman -S telegram-desktop --noconfirm;
 echo "telegram installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -636,11 +722,13 @@ sleep $SLEEP_TIME;
 
 
 # install_teams
-echo "\n"
+echo ""
+echo ""
 echo "installing ms teams"
 yay -S teams;
 echo "ms teams installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -652,11 +740,13 @@ sleep $SLEEP_TIME;
 
 
 # install_other_packages
-echo "\n"
+echo ""
+echo ""
 echo "installing other packages"
 sudo pacman -S ffmpeg tor variety vlc --noconfirm;
 echo "other packages installed"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 
@@ -669,19 +759,23 @@ sleep $SLEEP_TIME;
 # generic package management
 
 # Upgrade Packages
-echo "\n"
+echo ""
+echo ""
 echo "updating packages"
-sudo pacman -Syu;
+sudo pacman -Syu --noconfirm;
 echo "packages updated"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # Clean up all local caches. 
-echo "\n"
+echo ""
+echo ""
 echo "cleaning local caches"
 sudo pacman -Sc;
 echo "local caches cleaned"
-echo "\n"
+echo ""
+echo ""
 sleep $SLEEP_TIME;
 
 # Remove dependencies that are no longer needed
